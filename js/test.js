fbe89b5cf4029f58c1fdf2513c919afb020b5706
RidersWeb <49938053+RidersWeb@users.noreply.github.com>
2021-11-01 10:10:42 +0400

    Delete test.js

--- js/test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const btn = document.querySelector('button').addEventListener('mouseenter', event => {
-    console.log(event.target);
-    event.target.remove();
-});
-document.querySelector('.promo__adv-title').parentNode.remove();
-console.log(document.querySelector('.promo__adv-title').parentNode);
-
