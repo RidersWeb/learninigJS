381689fd65a8c28c5648e826d6bc95ff478c300d
RidersWeb <samara_expert@mail.ru>
2021-11-02 08:14:31 +0400

    add and delete film for object

--- js/script.js
+++ js/script.js
@@ -12,7 +12,27 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
+
+/* Задания на урок второй день:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
 'use strict';
+
+// Возьмите свой код из предыдущей практики
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -25,8 +45,12 @@ document.addEventListener('DOMContentLoaded', () => {
     // Все переменные
     const adv = document.querySelectorAll('.promo__adv img'),
           bg = document.querySelector('.promo__bg'),
-          bgImg = "url('/img/bg.jpg')", //прописываем адрес нового фона
-          movieList = document.querySelector('.promo__interactive-list');
+          bgImg = "url('./img/bg.jpg')", //прописываем адрес нового фона
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          addInput = document.querySelector('.adding__input'),
+          checkbox = document.querySelector('[type="checkbox"]');
+          
 
 const movieDB = {
     movies: [
@@ -40,7 +64,6 @@ const movieDB = {
         
     // Функция по замене фона    
     const bgChange = img => {
-        console.log(img);
         bg.style.backgroundImage = img;
     };
 
@@ -53,20 +76,44 @@ const movieDB = {
     });
 
     const createMoveList = (listFilm, parent) => {
-        movieList.innerText = '';
+        parent.innerText = '';
+        listFilm.sort();
         listFilm.forEach((film, i) => {
-            movieList.innerHTML += `
+            parent.innerHTML += `
             <li class="promo__interactive-item">${++i} - ${film}
             <div class="delete"></div>
             </li>`;
         });
 
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', event => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                createMoveList(movieDB.movies, movieList);
+            });
+        });
+    };
 
+    createMoveList(movieDB.movies, movieList);
 
+    //функция добавляет новый фильм
+    addForm.addEventListener('submit', event => {
+        event.preventDefault();
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
 
+        if(favorite) {
+            console.log('Любимый фильм');
+        }
 
-    }
-
-    createMoveList(movieDB.movies, parent);
-        
+        if(newFilm) {
+            if(newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+            movieDB.movies.push(newFilm);
+            createMoveList(movieDB.movies, movieList);
+        }
+    event.target.reset();
+    });
+    
 });
